1b2dc12890ca36e6d7bdfdffce8d29aae6fb3432
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-06 18:33:48 +0000

    Add movie service

---
+++ src/app/services/movieservice.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root', // can access the service throughout the whole application
+})
+export class Movieservice {
+  
+}
